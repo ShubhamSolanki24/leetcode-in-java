d47292b4092caff8b14540a8d630f5490ad08265
shubham solanki <49410636+ShubhamSolanki24@users.noreply.github.com>
2022-02-27 17:17:37 +0530

    Time: 6 ms (71.01%), Space: 62.7 MB (24.73%) - LeetHub

--- 658-find-k-closest-elements/658-find-k-closest-elements.java
+++ 658-find-k-closest-elements/658-find-k-closest-elements.java
@@ -1,6 +1,6 @@
 class Solution {
-   
-      public static class Pair implements Comparable <Pair>{
+   // TC -nlogk 
+  /*    public static class Pair implements Comparable <Pair>{
        int val;
        int gap;
        Pair(){
@@ -41,6 +41,25 @@ class Solution {
         Collections.sort(list);
 
         return list;
-     
-    }
+     */
+    
+    
+    //Optimised Approach
+      public List<Integer> findClosestElements(int[] arr, int k, int x) {
+          int l = 0;
+        int r = arr.length - k;
+        while (l < r) {
+            int mid = l + (r - l) / 2;
+            if (x - arr[mid] > arr[mid + k] - x)
+                l = mid + 1;
+            else 
+                r = mid;
+        }
+        List<Integer> res = new ArrayList<>();
+        for (int i = l; i < l + k; i++) {
+            res.add(arr[i]);
+        }
+        return res;
 }
+}
+    
