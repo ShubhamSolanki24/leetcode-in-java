11c036a778c0ad12b70423caaecaa3027737fe6b
shubham solanki <49410636+ShubhamSolanki24@users.noreply.github.com>
2022-02-27 21:02:21 +0530

    Time: 6 ms (9.76%), Space: 43.2 MB (34.66%) - LeetHub

--- 662-maximum-width-of-binary-tree/662-maximum-width-of-binary-tree.java
+++ 662-maximum-width-of-binary-tree/662-maximum-width-of-binary-tree.java
@@ -1,4 +1,4 @@
-
+/*
 
   public class TreeNode {
       int val;
@@ -12,27 +12,39 @@
           this.right = right;
       }
   }
-
+*/
 class Solution {
- record Pair(TreeNode node, int pos) {}
+ 
     public int widthOfBinaryTree(TreeNode root) {
-    var q = new ArrayDeque<>(List.of(new Pair(root, 0)));
-	var maxWidth = 1;
-
-	while (!q.isEmpty()) {
-		var head = q.peek();
-		var tail = head;
-
-		for (var i = q.size(); i > 0; i--) {
-			tail = q.poll();
-			if (tail.node.left != null)
-				q.add(new Pair(tail.node.left, 2 * tail.pos));
-			if (tail.node.right != null)
-				q.add(new Pair(tail.node.right, 2 * tail.pos + 1));
+    if(root==null || (root.left==null &&root.right==null)){
+			return 1;
 		}
-		maxWidth = Math.max(maxWidth, tail.pos - head.pos + 1);
-	}
-	return maxWidth;
+    int maxWidth = 0;
+		// <Nodee, Weight>
+		Map<TreeNode, Integer> map = new HashMap<>();
+		map.put(root, 0);
+		Queue<TreeNode> qu = new LinkedList<>();
+        
+        qu.offer(root);
+		while(!qu.isEmpty()){
+			int size = qu.size();
+			int left = map.get(qu.peek());
+			while(size-->0){
+				TreeNode head = qu.poll();
+				maxWidth = Math.max(maxWidth , map.get(head) - left+1);
+				if(head.left!=null){
+					qu.offer(head.left);
+					map.put(head.left, 2*map.get(head) );
+				}
 
+				if(head.right!=null){
+
+					qu.offer(head.right);
+					map.put(head.right, 2*map.get(head)+1 );
+				}
+			}
+		}
+		return maxWidth;
+    
     }
 }
