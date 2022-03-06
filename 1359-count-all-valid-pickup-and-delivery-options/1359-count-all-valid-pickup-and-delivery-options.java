2ba7a855248fd681ea3600aa70f04b75c7d87176
shubham solanki <49410636+ShubhamSolanki24@users.noreply.github.com>
2022-03-06 15:34:34 +0530

    Time: 1 ms (54.21%), Space: 41.1 MB (32.01%) - LeetHub

---
+++ 1359-count-all-valid-pickup-and-delivery-options/1359-count-all-valid-pickup-and-delivery-options.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int countOrders(int n) {
+       long[] dp  = new long[501];
+        int mod = (int)Math.pow(10,9)+7;
+        
+        dp[1] =1L;
+        dp[2] =6L;
+        for(int i =3;i<=n;i++){
+            int oddNo= 2*i-1;
+            int permu = oddNo *(oddNo +1)/2;
+            dp[i] =(dp[i-1] *(permu))%mod;
+        }
+        return (int)dp[n];
+    }
+}
