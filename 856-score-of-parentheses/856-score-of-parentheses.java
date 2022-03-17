1503e47a750f210e95ea57f97552de8dd5a45e5a
shubham solanki <49410636+ShubhamSolanki24@users.noreply.github.com>
2022-03-17 21:24:29 +0530

    Time: 1 ms (68.54%), Space: 41.8 MB (48.47%) - LeetHub

--- 856-score-of-parentheses/856-score-of-parentheses.java
+++ 856-score-of-parentheses/856-score-of-parentheses.java
@@ -1,24 +1,24 @@
 class Solution {
+  int i =0;
     public int scoreOfParentheses(String s) {
-        int count =0;
-        Stack<Integer> st = new Stack<>();
-        for(char c:s.toCharArray()){
-           int val =0;
-            if(c =='('){
-                st.push(0);
+   
+    int ans =0;
+     while(i<s.length()){
+         char first = s.charAt(i);i++;
+        if(first =='('){
+           
+            if(s.charAt(i) ==')'){
+                ans = ans+1;
+                i++;
+            }else{
+                ans = ans+ 2*(scoreOfParentheses(s));
             }
-             else{
-                 while(st.peek() != 0)
-                     val+= st.pop();
-                 
-                 val = Math.max(2*val,1);
-                 st.pop();
-                 st.push(val);
-                 
-             }
-    }
-    while(!st.isEmpty()) count+= st.pop();
-    return count;
+        }
+         else{
+             return ans;
+         }
+     }   
+        return ans;
     
     }  
 }
