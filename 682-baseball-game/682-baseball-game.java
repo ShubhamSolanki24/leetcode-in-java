2ab7d7e72dd1ad1539403c41f614eb2c685b76db
shubham solanki <49410636+ShubhamSolanki24@users.noreply.github.com>
2022-04-10 13:43:52 +0530

    Time: 3 ms (78.45%), Space: 43.3 MB (5.24%) - LeetHub

--- 682-baseball-game/682-baseball-game.java
+++ 682-baseball-game/682-baseball-game.java
@@ -1,6 +1,6 @@
 class Solution {
     public int calPoints(String[] ops) {
-      ArrayList<Integer> scores = new ArrayList<>();
+      /* ArrayList<Integer> scores = new ArrayList<>();
           for(String el: ops) {
             final int lastIndex = scores.size() - 1;
             int result;
@@ -25,6 +25,27 @@ class Solution {
         
         int res = 0;
         for(int el: scores) { res += el; }
-        return res;  
+        return res; */ 
+        
+        Stack<Integer> stack = new Stack();
+
+        for(String op : ops) {
+            if (op.equals("+")) {
+                int top = stack.pop();
+                int newtop = top + stack.peek();
+                stack.push(top);
+                stack.push(newtop);
+            } else if (op.equals("C")) {
+                stack.pop();
+            } else if (op.equals("D")) {
+                stack.push(2 * stack.peek());
+            } else {
+                stack.push(Integer.valueOf(op));
+            }
+        }
+
+        int ans = 0;
+        for(int score : stack) ans += score;
+        return ans;
     }
 }
