f5ffb86b25f7937093a618404559e9ee4da68dd7
shubham solanki <49410636+ShubhamSolanki24@users.noreply.github.com>
2022-04-21 23:20:30 +0530

    Time: 831 ms (8.36%), Space: 56.3 MB (67.49%) - LeetHub

--- 705-design-hashset/705-design-hashset.java
+++ 705-design-hashset/705-design-hashset.java
@@ -1,5 +1,5 @@
 class MyHashSet {
-  boolean[] set =null;
+ /* boolean[] set =null;
     public MyHashSet() {
    set= new boolean[1000001];        
     }
@@ -15,8 +15,31 @@ class MyHashSet {
     public boolean contains(int key) {
         return set[key];
     }
-}
+    
+    */
+    
+      ArrayList<Integer> arr;
 
+    public MyHashSet() {
+        arr = new ArrayList<>();
+    }
+    
+    public void add(int key) {
+         if(!arr.contains(key)){               // add only if the array doesnt contain the same key
+             arr.add(key);
+         }
+    }
+    
+    public void remove(int key) {
+        if(arr.contains(key)){
+            arr.remove(arr.indexOf(key));               // remove only if array contains that key
+        }
+    }
+    
+    public boolean contains(int key) {
+        return arr.contains(key);
+}
+}
 /**
  * Your MyHashSet object will be instantiated and called as such:
  * MyHashSet obj = new MyHashSet();
