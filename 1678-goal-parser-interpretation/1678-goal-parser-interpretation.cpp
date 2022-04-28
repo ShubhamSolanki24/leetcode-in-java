1d52a3c6e4c3eb8cda29a6615fc80c45196936d0
Shubham Solanki <49410636+ShubhamSolanki24@users.noreply.github.com>
2022-04-28 12:07:24 +0530

    Time: 0 ms (100.00%), Space: 6 MB (81.39%) - LeetHub

--- 1678-goal-parser-interpretation/1678-goal-parser-interpretation.cpp
+++ 1678-goal-parser-interpretation/1678-goal-parser-interpretation.cpp
@@ -1,18 +1,26 @@
 class Solution {
 public:
     string interpret(string command) {
-      int N = command.length();
-       string s; 
-       for(int i=0;i<N;i++){
-           if(command[i]=='G'){
-                s.push_back('G');
-           }else if(command[i] =='(' && command[i+1]==')'){
-                 s.push_back('o');
-           }else if(command[i] =='(' && command[i+1]=='a' && command[i+2]=='l' && command[i+3]==')'){
-                 s.push_back('a');
-                 s.push_back('l');
-           }
-       } 
-      return s;  
+      int i=0;
+        string s;
+        while(i<command.size())
+        {
+            if(command[i]=='G')
+            {
+                s.append("G");
+                i++;
+            }
+            else if(command[i]=='(' && command[i+1]==')')
+            {
+                s.append("o");
+                i+=2;
+            }
+            else
+            {
+                s.append("al");
+                i+=4;
+            }
+        }
+        return s;  
     }
 };
