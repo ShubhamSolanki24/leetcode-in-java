99c401c1858ee934b3475723eabccce49b1a24d6
Shubham Solanki <49410636+ShubhamSolanki24@users.noreply.github.com>
2022-04-29 23:17:13 +0530

    Time: 1 ms (83.33%), Space: 53.5 MB (58.05%) - LeetHub

--- 785-is-graph-bipartite/785-is-graph-bipartite.java
+++ 785-is-graph-bipartite/785-is-graph-bipartite.java
@@ -12,7 +12,7 @@ class Solution {
     }
     //dfs 
     public boolean validColor(int[][] g,int[] color,int c,int node){
-        if(color[node]!=0)
+        if(color[node]!=0)https://leetcode.com/problems/is-graph-bipartite/
             return color[node]==c;
         color[node] =c;
         for(int n : g[node]){
@@ -22,8 +22,8 @@ class Solution {
     }   */
     
    // BFS
-    public boolean isBipartite(int[][] graph) {
-        int [] color = new int[graph.length];
+  /*  public boolean isBipartite(int[][] graph) {
+         int [] color = new int[graph.length];
         for(int i =0;i<graph.length;i++){
             if(color[i] ==0){
             Queue<Integer> q = new LinkedList<>();
@@ -42,6 +42,27 @@ class Solution {
           }
         }
         return true;  
-    }
-    
+    }  */
+    public boolean isBipartite(int[][] graph){
+        int[] color = new int[graph.length];
+         
+        for(int i =0;i<graph.length;i++){
+           if(color[i] == 1|| color[i] ==-1)continue;
+            Queue<Integer> q = new LinkedList<>();
+            q.add(i);
+            color[i] =1;
+        
+            while(!q.isEmpty()){
+                int cur = q.poll();
+                for(int next:graph[cur]){
+                    if(color[next] ==0){
+                       color[next] = -color[cur];
+                        q.add(next);
+                    }
+                    if(color[next] ==color[cur])return false;
+                }
+            }
+        }
+        return true;
+    } 
 }
