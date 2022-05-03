65f7bc828cfef9ebad7e6a3e9bf4359aadd4c27e
Shubham Solanki <49410636+ShubhamSolanki24@users.noreply.github.com>
2022-05-03 20:44:55 +0530

    Time: 3 ms (69.35%), Space: 43.1 MB (59.00%) - LeetHub

--- 415-add-strings/415-add-strings.java
+++ 415-add-strings/415-add-strings.java
@@ -1,20 +1,24 @@
 class Solution {
     public String addStrings(String num1, String num2) {
-       String res ="";
-        int i =num1.length() -1;
-        int j = num2.length()-1;
-        int carry =0 ;
-        
-        while(i>=0||j>=0|| carry!=0){
-            int ival =i>=0 ? num1.charAt(i) -'0': 0;
-            int jval =j>=0 ? num2.charAt(j) -'0': 0;
-            i--;
-            j--;
-            int sum = ival +jval +carry;
-            res = (sum %10) +res;
-            carry = sum/10;
+  StringBuilder res = new StringBuilder();
+
+        int carry = 0;
+        int p1 = num1.length() - 1;
+        int p2 = num2.length() - 1;
+        while (p1 >= 0 || p2 >= 0) {
+            int x1 = p1 >= 0 ? num1.charAt(p1) - '0' : 0;
+            int x2 = p2 >= 0 ? num2.charAt(p2) - '0' : 0;
+            int value = (x1 + x2 + carry) % 10;
+            carry = (x1 + x2 + carry) / 10;
+            res.append(value);
+            p1--;
+            p2--;    
         }
-        return res;
+        
+        if (carry != 0)
+            res.append(carry);
+        
+        return res.reverse().toString();
         }
 
 }
