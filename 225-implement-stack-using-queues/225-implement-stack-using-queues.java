eb068c83fbd94c6f9a7ccdab94a1a92b7d582a01
Shubham Solanki <49410636+ShubhamSolanki24@users.noreply.github.com>
2022-05-05 22:50:32 +0530

    Time: 0 ms (100.00%), Space: 42.3 MB (13.64%) - LeetHub

--- 225-implement-stack-using-queues/225-implement-stack-using-queues.java
+++ 225-implement-stack-using-queues/225-implement-stack-using-queues.java
@@ -24,13 +24,5 @@ class MyStack {
     public boolean empty() {
         return q.isEmpty();
     }
-}
-
-/**
- * Your MyStack object will be instantiated and called as such:
- * MyStack obj = new MyStack();
- * obj.push(x);
- * int param_2 = obj.pop();
- * int param_3 = obj.top();
- * boolean param_4 = obj.empty();
- */
+} 
+   
