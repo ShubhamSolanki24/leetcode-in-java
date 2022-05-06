9bffa12b23f35944f554aa6acfc2a6ee8d271025
Shubham Solanki <49410636+ShubhamSolanki24@users.noreply.github.com>
2022-05-06 14:57:03 +0530

    Time: 34 ms (99.64%), Space: 61.9 MB (69.18%) - LeetHub

--- 853-car-fleet/853-car-fleet.java
+++ 853-car-fleet/853-car-fleet.java
@@ -1,5 +1,6 @@
 class Solution {
     public int carFleet(int target, int[] position, int[] speed) {
+       /*
        int n = position.length; 
         double[][] cardata = new  double[n][2];
         //0th position 
@@ -21,6 +22,27 @@ class Solution {
         }
         
         return nof;
+        */
+          int res = 0;
+           double[] timeArr = new double[target];
+           for (int i = 0; i < position.length; i++)
+            {
+                timeArr[position[i]]= (double)(target - position[i]) / speed[i];
+            }
+            double prev = 0.0;
+            for (int i = target-1; i >=0 ; i--)
+            {
+                double cur = timeArr[i];
+                if (cur > prev)
+                {
+                    prev = cur;
+                    res++;
+                }
+            }
+            return res;
+        
+        
+        
         
         
     }
