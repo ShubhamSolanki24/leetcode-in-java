71924b6c77bfb7d55aa56f93d97f07ae196fd18d
Shubham Solanki <49410636+ShubhamSolanki24@users.noreply.github.com>
2022-05-07 23:34:48 +0530

    Time: 2 ms (99.06%), Space: 99 MB (95.72%) - LeetHub

--- 41-first-missing-positive/41-first-missing-positive.java
+++ 41-first-missing-positive/41-first-missing-positive.java
@@ -1,6 +1,6 @@
 class Solution {
     public int firstMissingPositive(int[] nums) {
-        Set<Integer> set = new HashSet();
+      /*  Set<Integer> set = new HashSet();
         for(int num :nums){
             set.add(num);
         }
@@ -9,5 +9,21 @@ class Solution {
             i++;
         }
         return i;
+        */
+        
+         boolean[] present = new boolean[nums.length];
+        for (int num : nums) {
+            if (num > 0 && num <= nums.length) {
+                present[num - 1] = true;
+            }
+        }
+        for (int i = 0; i < present.length; i++) {
+            if (!present[i]) {
+                return i + 1;
+            }
+        }
+        return nums.length + 1;
+        
+        
     }
 }
