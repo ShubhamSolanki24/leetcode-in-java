82bc41a378aaa492f39be0ecff159d1f41df032c
Shubham Solanki <49410636+ShubhamSolanki24@users.noreply.github.com>
2022-05-09 21:00:01 +0530

    Time: 8 ms (85.91%), Space: 50.8 MB (78.30%) - LeetHub

--- 148-sort-list/148-sort-list.java
+++ 148-sort-list/148-sort-list.java
@@ -28,31 +28,27 @@ class Solution {
     }
     public ListNode  merge(ListNode list1,ListNode list2){
           ListNode dummyHead = new ListNode();
-        ListNode tail = dummyHead;
+        ListNode curr = dummyHead;
         while (list1 != null && list2 != null) {
             if (list1.val < list2.val) {
-                tail.next = list1;
+                curr.next = list1;
                 list1 = list1.next;
-                tail = tail.next;
+                curr = curr.next;
             } else {
-                tail.next = list2;
+                curr.next = list2;
                 list2 = list2.next;
-                tail = tail.next;
+                curr = curr.next;
             }
         }
-        tail.next = (list1 != null) ? list1 : list2;
+        if(list1!=null){
+            curr.next = list1;
+            list1= list1.next;
+        }
+        if(list2!=null){
+            curr.next =list2;
+            list2= list2.next;
+        }
         return dummyHead.next;
     }
 
-    ListNode getMid(ListNode head) {
-        ListNode midPrev = null;
-        while (head != null && head.next != null) {
-            midPrev = (midPrev == null) ? head : midPrev.next;
-            head = head.next.next;
-        }
-        ListNode mid = midPrev.next;
-        midPrev.next = null;
-        return mid;
-    }
-    
 }
