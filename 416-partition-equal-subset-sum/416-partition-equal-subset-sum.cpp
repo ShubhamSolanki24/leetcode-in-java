5b64700fbb80c6578f78e26bdfc896287649b827
Shubham Solanki <49410636+ShubhamSolanki24@users.noreply.github.com>
2022-06-04 10:39:32 +0530

    Time: 252 ms (50.96%), Space: 11.2 MB (73.80%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
@@ -1,24 +1,36 @@
 class Solution {
 public:
-    int8_t dp[10001] = {[0 ... 10000] = -1};
-    bool canPartition(vector<int>& nums) {
-         int n = nums.size();
-        int sum =0;
-      for(int i=0;i<n;i++){
-          sum += nums[i];
-      } 
-      if(sum%2 != 0){
-          return false;
-      }else {
-          return subsetSum(nums,sum/2);
-      }
+
+bool subset(vector<int>&arr,int sum){
+    int n = arr.size();
+    bool a[n+1][sum+1];
+    for(int i=0;i<sum+1;i++)
+        {
+            a[0][i]=false;
+        }
+        for(int i=0;i<n+1;i++)
+        {
+            a[i][0]=true;
+        }
+    for(int i =1;i<n+1;i++){
+        for(int j =1;j<(sum+1);j++){
+            if(arr[i-1]<=j){
+                a[i][j] = (a[i-1][j-arr[i-1]] || a[i-1][j]);
+            }else{
+                a[i][j] = a[i-1][j];
+            }
+        }
     }
-    
-    bool subsetSum(vector<int>& nums, int sum, int i =0){
-    
-        if(sum == 0) return true;
-        if(i >= size(nums) || sum < 0) return false; 
-        if(dp[sum] != -1) return dp[sum];
-        return dp[sum] = subsetSum(nums, sum - nums[i], i + 1) || subsetSum(nums, sum, i + 1);
+    return a[n][sum];
+}
+bool canPartition(vector<int>& arr) {
+int n = arr.size();
+    int sum =0;
+    for(int i =0;i<n;i++){
+        sum+=arr[i];
     }
-};
+    if(sum%2!=0){
+        return false;
+    }
+    return subset(arr,sum/2);
+}};
