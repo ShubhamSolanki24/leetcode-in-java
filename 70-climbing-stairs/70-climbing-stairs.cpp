7318ccc8e420ae39c6657d9b7f51b62a9a057469
Shubham Solanki <49410636+ShubhamSolanki24@users.noreply.github.com>
2022-06-05 19:31:42 +0530

    Time: 4 ms (21.19%), Space: 6.3 MB (15.36%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.cpp
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
-      int helper(vector<int> &dp,int n){
+  /*recursive solution
+    int helper(vector<int> &dp,int n){
         if(n<0) return 0;
         if(n==0 || n==1)    return 1;
         if(dp[n]!=-1)   return dp[n];
@@ -10,5 +11,14 @@ public:
         vector<int> dp(n+1,-1);
         return helper(dp,n);
     }
-   
+   */
+    
+    int climbStairs(int n){
+     vector<int> dp(n+1);
+        dp[0]=dp[1]=1;
+        for(int i=2;i<=n;i++)   
+            dp[i]=dp[i-1]+dp[i-2];
+        return dp[n];
+    }
+    
 };
