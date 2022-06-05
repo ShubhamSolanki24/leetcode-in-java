c08464aa76f38b71084b701cfcbb5cfca0c4de9b
Shubham Solanki <49410636+ShubhamSolanki24@users.noreply.github.com>
2022-06-05 19:33:05 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (15.36%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.cpp
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -16,8 +16,13 @@ public:
     int climbStairs(int n){
      vector<int> dp(n+1);
         dp[0]=dp[1]=1;
+        
         for(int i=2;i<=n;i++)   
+        if(dp[i]!=-1){
             dp[i]=dp[i-1]+dp[i-2];
+        }else{
+            dp[i]= dp[i];
+        }
         return dp[n];
     }
     
