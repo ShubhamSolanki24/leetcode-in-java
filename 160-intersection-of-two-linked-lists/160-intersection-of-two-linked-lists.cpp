9b59b30a44d4d81002702a973531436c277e71de
Shubham Solanki <49410636+ShubhamSolanki24@users.noreply.github.com>
2022-06-06 23:40:01 +0530

    Time: 51 ms (77.87%), Space: 14.7 MB (31.40%) - LeetHub

--- 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.cpp
+++ 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.cpp
@@ -9,7 +9,57 @@
 class Solution {
 public:
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
-    ListNode* a = headA,*b = headB;
+    int len1=0,len2=0;
+        ListNode* curr = headA;
+        
+        while(curr){
+            ++len1;
+            curr = curr->next;
+        }
+        
+        curr = headB;
+
+        while(curr){
+            ++len2;
+            curr = curr->next;
+        }
+        
+        if(len1>=len2){
+            
+            int d = len1-len2;
+            ListNode* fast = headA;
+            ListNode* slow = headB; 
+            while(d--)
+                fast=fast->next;
+            
+            while(fast!=slow){
+                fast = fast->next;
+                slow = slow->next;
+            }
+            
+            return slow;
+        }
+        else{
+            
+            int d = len2-len1;
+            ListNode* slow = headA;
+            ListNode* fast = headB; 
+            while(d--)
+                fast=fast->next;
+            
+            while(fast!=slow){
+                fast = fast->next;
+                slow = slow->next;
+            }
+            
+            return slow;
+            
+        }
+        
+        return nullptr;
+        
+        
+        /* ListNode* a = headA,*b = headB;
         while(a!=b){
             if(a==NULL)a =headB;
             else a= a->next;
@@ -18,6 +68,6 @@ public:
             else b= b->next;
         }
         return a;
-        
+        */
     }
 };
