a3369d570cf573c9be8ddd6a5e67e3b1934e0778
Shubham Solanki <49410636+ShubhamSolanki24@users.noreply.github.com>
2022-06-07 12:41:56 +0530

    Time: 162 ms (36.80%), Space: 67.8 MB (54.40%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.cpp
+++ 53-maximum-subarray/53-maximum-subarray.cpp
@@ -1,15 +1,16 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-        int curr=nums[0],global=nums[0],n=nums.size();
-    
-    for(int i=1;i<n;i++){
+        int maxVal = nums[0], curSum = 0;
+        int const n = nums.size();
         
-        curr = max(curr+nums[i],nums[i]);               //curr = maximum of sum till current user and current number
-        global = max(global,curr);                      // global = max of global and current sum
+        for (int i = 0; i < n; i++) {
+            curSum += nums[i];
+            if(curSum > maxVal) 
+                maxVal = curSum;
+            if(curSum < 0) curSum = 0;
+        }
         
+        return maxVal;
     }
-        return global;
-    }
-
 };
